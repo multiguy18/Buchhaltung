40681396f8587871583e0ad87292a98efa226eb2
bannwafa <33682594+bannwafa@users.noreply.github.com>
2019-06-12 12:01:06 +0200

    T erstellt

---
+++ Buchhaltung_Fabian_Zaniar_Noah_Amsel/Buchhaltung_Fabian_Zaniar_Noah_Amsel/View/KontenView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Buchhaltung_Fabian_Zaniar_Noah_Amsel.View
+{
+    /// <summary>
+    /// Interaction logic for KontenView.xaml
+    /// </summary>
+    public partial class KontenView : Window
+    {
+        public KontenView()
+        {
+            InitializeComponent();
+        }
+    }
+}
