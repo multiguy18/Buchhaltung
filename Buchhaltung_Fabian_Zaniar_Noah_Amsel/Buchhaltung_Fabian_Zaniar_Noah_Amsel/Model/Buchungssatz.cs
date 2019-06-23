e9994242eeb0d1115208f340a08578ac26d21b37
multiguy18 <anselm1793@hotmail.com>
2019-06-23 20:53:55 +0200

    Klassen für Hintergrundlogik hinzugefügt

---
+++ Buchhaltung_Fabian_Zaniar_Noah_Amsel/Buchhaltung_Fabian_Zaniar_Noah_Amsel/Model/Buchungssatz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buchhaltung_Fabian_Zaniar_Noah_Amsel.Model
+{
+    public class Buchungssatz
+    {
+        public int Nummer; //Brauchts das?
+        public Konto Soll;
+        public Konto Haben;
+        public float Betrag;
+        public DateTime Zeitpunkt;
+
+        public bool Kalkuliere()
+        {
+            //TODO: Logik für Passiv- bzw. Aufwandskonto
+            if (Soll.DoUebertrag(Betrag) && Haben.DoUebertrag(Betrag))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Buchhaltung_Fabian_Zaniar_Noah_Amsel/Buchhaltung_Fabian_Zaniar_Noah_Amsel/Model/Konto.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buchhaltung_Fabian_Zaniar_Noah_Amsel.Model
+{
+    public class Konto
+    {
+        public readonly bool PassivAufwand;
+        public readonly string Bezeichnung;
+        private float _anfangsbestand;
+        private List<float> _aeanderungen;
+
+        public Konto(string bezeichnung, bool passivAufwand)
+        {
+            Bezeichnung = bezeichnung;
+            PassivAufwand = passivAufwand;
+        }
+
+        public float Schlussbestand { get; private set; }
+
+        public float Anfangsbestand
+        {
+            get { return _anfangsbestand; }
+            set
+            {
+                _anfangsbestand = value;
+                ResetUebertraege();
+            }
+        }
+
+
+        public List<float> Aenderungen
+        {
+            get { return new List<float>(_aeanderungen); }
+        }
+
+        public bool DoUebertrag(float betrag)
+        {
+            _aeanderungen.Add(betrag);
+
+            Schlussbestand += betrag;
+
+            if (Schlussbestand < 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void ResetUebertraege()
+        {
+            _aeanderungen.Clear();
+            Schlussbestand = _anfangsbestand;
+        }
+    }
+}
